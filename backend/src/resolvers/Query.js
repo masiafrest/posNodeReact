16215c8d0bf19f233c34633e3a33b10c7ca2ae6a
masiafrest <masiafrest@gmail.com>
2021-06-05 11:23:23 -0600

    add query categorias and ubicaciones and resolver on those

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,18 +1,37 @@
 /**
  * @typedef { import("@prisma/client").PrismaClient } Prisma
- * @param {any} parent
- * @param {{ searchString: string }} args
- * @param {{ prisma: Prisma }} ctx
  */
+
 function items(parent, args, ctx, info) {
   return ctx.prisma.item.findMany({
     include: {
       categorias: true,
-      inventarios: { include: { precio: true, ubicacion: true } },
+      inventarios: {
+        include: {
+          precio: true,
+          ubicacion: true
+        }
+      },
     },
   });
 }
 
+/** 
+ * @typedef { import("@prisma/client").PrismaClient } Prisma
+ * @param {any} parent
+ * @param {{ searchString: string }} args
+ * @param {{ prisma: Prisma }} ctx
+ */
+function categorias(parent, args, ctx, info) {
+  return ctx.prisma.categoria.findMany()
+}
+
+function ubicaciones(parent, args, ctx, info) {
+  return ctx.prisma.ubicacion.findMany()
+}
+
 module.exports = {
   items,
+  categorias,
+  ubicaciones
 };
