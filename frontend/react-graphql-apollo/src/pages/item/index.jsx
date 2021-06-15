3a1740ef765dd515ec1485fd7bc5a582f915e7f6
masiafrest <masiafrest@gmail.com>
2021-06-15 18:13:47 -0500

    create item

--- frontend/react-graphql-apollo/src/pages/item/index.jsx
+++ frontend/react-graphql-apollo/src/pages/item/index.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import ItemList from "./components/ItemList";
+import ItemCreateDialogIcon from "./components/ItemCreateDialogIcon";
 
-  export const ITEM_DATA = gql`
-    fragment itemData on Item{
-              id
-              marca
-              modelo
-              barcode
-              sku
-              descripcion
-              qty
-              categorias {
-                id
-                nombre
-              }
-              precio {
-                precio
-                precioMin
-              }
-              ubicacion {
-                id
-                tipo
-                dirrecion
-              }
-    } 
-  `
+export const ITEM_DATA = gql`
+  fragment itemData on Item {
+    id
+    marca
+    modelo
+    barcode
+    sku
+    descripcion
+    qty
+    categorias {
+      id
+      nombre
+    }
+    precio {
+      precio
+      precioMin
+    }
+    ubicacion {
+      id
+      tipo
+      dirrecion
+    }
+  }
+`;
 
 export default function Item() {
   const ITEM_QUERY = gql`{
@@ -43,6 +44,7 @@ export default function Item() {
     <>
       <div>item pages</div>;
       {loading ? <div>loading</div> : <ItemList items={data.items} />}
+      <ItemCreateDialogIcon />
     </>
   );
 }
