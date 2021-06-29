9b146c82aa3d8f2449361cca7f9344ef7bd436d1
masiafrest <masiafrest@gmail.com>
2021-06-29 17:42:30 -0500

    adding pagination

--- frontend/react-graphql-apollo/src/App.js
+++ frontend/react-graphql-apollo/src/App.js
@@ -3,7 +3,7 @@ import "./styles/App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavBar from "./components/Navbar";
-import Item from './pages/item'
+import Item from "./pages/item";
 
 function App() {
   return (
@@ -11,7 +11,8 @@ function App() {
       <NavBar />
       <Switch>
         <Route path="/home">home</Route>
-        <Route path="/item" component={Item}/>
+        <Route path="/item/" component={Item} />
+        <Route path="/item/new/:page" component={Item} />
       </Switch>
     </Router>
   );
--- frontend/react-graphql-apollo/src/pages/item/index.jsx
+++ frontend/react-graphql-apollo/src/pages/item/index.jsx
@@ -4,11 +4,41 @@ import { GET_ITEMS } from "./graphql/query";
 import ItemList from "./components/ItemList";
 import CreateItemDialog from "./components/ItemDialog/";
 import FilterBar from "./components/FilterBar";
+import { useHistory } from "react-router-dom";
+import { Container, Grid, Button } from "@material-ui/core";
 
 export default function Item() {
+  const ITEMS_PER_PAGE = 2;
+  const history = useHistory();
+  const isNewPage = history.location.pathname.includes("new");
+  const pageIndexParams = history.location.pathname.split("/");
+  const page = parseInt(pageIndexParams.length - (isNewPage ? 2 : 1));
+  const pageIndex = page ? (page - 1) * ITEMS_PER_PAGE : 0;
+  console.log("isnewpate:", isNewPage);
+  console.log("pageIndexParams:", pageIndexParams);
+  console.log("page:", page);
+  console.log("pageInex:", pageIndex);
+
+  const getQueryVariables = (isNewPage, page) => {
+    const skip = isNewPage ? (page - 1) * ITEMS_PER_PAGE : 0;
+    const take = ITEMS_PER_PAGE;
+    const orderBy = { createdAt: "desc" };
+    return { take, skip, orderBy };
+  };
+
+  const getItemsToRender = (isNewPage, data) => {
+    if (isNewPage) {
+      return data.items;
+    }
+    const rankedItems = data.items.slice();
+    // rankedItems.sort((l1, l2) => l2.votes.length - l1.votes.length);
+    return rankedItems;
+  };
+
   const [filter, setFilter] = useState("");
-  const [take, setTake] = useState(5);
-  const [skip, setSkip] = useState(0);
+  // const [take, setTake] = useState(5);
+  // const [skip, setSkip] = useState(0);
+  const { take, skip } = getQueryVariables(isNewPage, page);
 
   const { data, loading, error } = useQuery(GET_ITEMS, {
     variables: { filter, take, skip },
@@ -27,11 +57,63 @@ export default function Item() {
         //filtro y barra de busqueda
       }
       {data ? (
-        <ItemList items={data.items} />
+        <ItemList items={getItemsToRender(isNewPage, data)} />
       ) : (
         <div> data undefined puede que no este conectado al servidor</div>
       )}
       <CreateItemDialog />
+
+      {isNewPage ? (
+        <Container>
+          <Grid container>
+            <Grid item>
+              <Button
+                onClick={() => {
+                  if (page === 2) {
+                    history.push(`/item`);
+                  }
+                  if (page > 2) {
+                    history.push(`/new/${page - 1}`);
+                  }
+                }}
+              >
+                previous
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                onClick={() => {
+                  console.log("next data", data);
+                  if (page <= data?.items.length / ITEMS_PER_PAGE) {
+                    const nextPage = page + 1;
+                    console.log("next");
+                    history.push(`item/new/${nextPage}`);
+                  }
+                }}
+              >
+                next
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      ) : (
+        <Grid container>
+          <Grid item>
+            <Button
+              onClick={() => {
+                console.log("next data", data);
+                if (page <= data?.items.length / ITEMS_PER_PAGE) {
+                  const nextPage = page + 1;
+                  console.log("next");
+                  history.push(`item/new/${nextPage}`);
+                }
+              }}
+            >
+              next
+            </Button>
+          </Grid>
+        </Grid>
+      )}
     </>
   );
 }
