374993e051fc9a202a5e1cee1cf642c6d51e9645
masiafrest <masiafrest@gmail.com>
2021-06-30 18:48:11 -0500

    move logic to item list

--- frontend/react-graphql-apollo/src/pages/item/components/ItemList/index.jsx
+++ frontend/react-graphql-apollo/src/pages/item/components/ItemList/index.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ITEMS } from "../../graphql/query";
 import ItemAccordion from "./ItemAccordion";
-import { Grid } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { Container, Grid } from "@material-ui/core";
+import BtnNextPrevious from "./BtnNextPrevious";
 
-export default function ItemList({ items }) {
-  console.log(items);
+export default function ItemList({ filter }) {
+  const ITEMS_PER_PAGE = 2;
+  const history = useHistory();
+  const isNewPage = history.location.pathname.includes("new");
+  const pageIndexParams = history.location.pathname.split("/");
+  const page = parseInt(
+    isNewPage ? pageIndexParams[pageIndexParams.length - 1] : 1
+  );
+
+  const getQueryVariables = (isNewPage, page) => {
+    const skip = isNewPage ? (page - 1) * ITEMS_PER_PAGE : 0;
+    const take = ITEMS_PER_PAGE;
+    const orderBy = { createdAt: "desc" };
+    return { take, skip, orderBy };
+  };
+  const { take, skip } = getQueryVariables(isNewPage, page);
+  console.log("isNewPage", isNewPage);
+  console.log("pageIndexParams", pageIndexParams);
+  console.log("page", page);
+  console.log("skip", skip);
+  const { data, loading, error } = useQuery(GET_ITEMS, {
+    variables: { filter, take, skip },
+  });
+
+  if (loading) return <div>loading</div>;
+  if (error) return `${error}`;
   //TODO add grid, a swipable to del, maybe a materialUiContainer too
   return (
-    <Grid container spacing={1}>
-      {items.map((item) => (
-        <Grid item key={`item-grid-${item.id}`}>
-          <ItemAccordion item={item} key={`item-${item.id}`} />
+    <>
+      <Grid container spacing={1}>
+        {data.items.map((item) => (
+          <Grid item key={`item-grid-${item.id}`}>
+            <ItemAccordion item={item} key={`item-${item.id}`} />
+          </Grid>
+        ))}
+      </Grid>
+      <Container>
+        <Grid container>
+          {isNewPage && (
+            <Grid item>
+              <BtnNextPrevious isNext={false} page={page} history={history} />
+            </Grid>
+          )}
+          {data?.items.length >= ITEMS_PER_PAGE && (
+            <Grid item>
+              <BtnNextPrevious isNext={true} page={page} history={history} />
+            </Grid>
+          )}
         </Grid>
-      ))}
-    </Grid>
+      </Container>
+    </>
   );
 }
--- frontend/react-graphql-apollo/src/pages/item/index.jsx
+++ frontend/react-graphql-apollo/src/pages/item/index.jsx
@@ -1,72 +1,10 @@
 import { useState } from "react";
-import { useQuery } from "@apollo/client";
-import { GET_ITEMS } from "./graphql/query";
 import ItemList from "./components/ItemList";
 import CreateItemDialog from "./components/ItemDialog/";
 import FilterBar from "./components/FilterBar";
-import { useHistory } from "react-router-dom";
-import { Container, Grid, Button } from "@material-ui/core";
 
 export default function Item() {
-  const ITEMS_PER_PAGE = 2;
-  const history = useHistory();
-  const isNewPage = history.location.pathname.includes("new");
-  const pageIndexParams = history.location.pathname.split("/");
-  const page = parseInt(
-    isNewPage ? pageIndexParams[pageIndexParams.length - 1] : 1
-  );
-  console.log("isnewpate:", isNewPage);
-  console.log("pageIndexParams:", pageIndexParams);
-  console.log("page:", page);
-
-  const BtnNextPrevious = ({ isNext }) => {
-    const previousHandler = () => {
-      if (page === 2) {
-        history.push(`/item`);
-      }
-      if (page > 2) {
-        history.push(`item/new/${page - 1}`);
-      }
-    };
-    const nextHandler = () => {
-      history.push(`item/new/${page + 1}`);
-    };
-    return (
-      <Button
-        variant="outlined"
-        onClick={isNext ? nextHandler : previousHandler}
-      >
-        {isNext ? "next" : "previous"}
-      </Button>
-    );
-  };
-
-  const getQueryVariables = (isNewPage, page) => {
-    const skip = isNewPage ? (page - 1) * ITEMS_PER_PAGE : 0;
-    const take = ITEMS_PER_PAGE;
-    const orderBy = { createdAt: "desc" };
-    return { take, skip, orderBy };
-  };
-
-  const getItemsToRender = (isNewPage, data) => {
-    if (isNewPage) {
-      return data.items;
-    }
-    const rankedItems = data.items.slice();
-    // rankedItems.sort((l1, l2) => l2.votes.length - l1.votes.length);
-    return rankedItems;
-  };
-
   const [filter, setFilter] = useState("");
-  const { take, skip } = getQueryVariables(isNewPage, page);
-
-  const { data, loading, error } = useQuery(GET_ITEMS, {
-    variables: { filter, take, skip },
-  });
-  console.log("data", data);
-
-  if (loading) return <div>loading</div>;
-  if (error) return `${error}`;
 
   return (
     <>
@@ -76,33 +14,8 @@ export default function Item() {
       {
         //filtro y barra de busqueda
       }
-      {data ? (
-        <ItemList items={getItemsToRender(isNewPage, data)} />
-      ) : (
-        <div> data undefined puede que no este conectado al servidor</div>
-      )}
+      <ItemList filter={filter} />
       <CreateItemDialog />
-
-      {isNewPage ? (
-        <Container>
-          <Grid container>
-            <Grid item>
-              <BtnNextPrevious isNext={false} />
-            </Grid>
-            <Grid item>
-              {data?.items.length >= ITEMS_PER_PAGE && (
-                <BtnNextPrevious isNext={true} />
-              )}
-            </Grid>
-          </Grid>
-        </Container>
-      ) : (
-        <Grid container>
-          <Grid item>
-            <BtnNextPrevious isNext={true} />
-          </Grid>
-        </Grid>
-      )}
     </>
   );
 }
