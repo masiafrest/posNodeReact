5f862a6496582ee4baf51d134b2d003c701b5316
masiafrest <masiafrest@gmail.com>
2021-07-05 05:52:32 -0400

    add import

--- frontend/react-graphql-apollo/src/pages/recibos/venta/index.jsx
+++ frontend/react-graphql-apollo/src/pages/recibos/venta/index.jsx
@@ -1,4 +1,9 @@
 import ClientSelect from "./components/ClientSelect";
+// import { useSelector } from "react-redux";
+// import { useSnackbar } from "notistack";
+// import ReactToPrint from "react-to-print";
+// import ComponentToPrint from "../../components/ComponentToPrint";
+
 export default function Venta() {
   return <ClientSelect />;
 }
