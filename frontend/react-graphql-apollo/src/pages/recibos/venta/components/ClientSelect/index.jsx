9e0ffe0991c3ef883f7c811313f55bc4ca5ea327
masiafrest <masiafrest@gmail.com>
2021-07-06 15:47:11 -0500

    fix query get client

--- frontend/react-graphql-apollo/src/pages/recibos/venta/components/ClientSelect/index.jsx
+++ frontend/react-graphql-apollo/src/pages/recibos/venta/components/ClientSelect/index.jsx
@@ -6,10 +6,10 @@ import debounce from "lodash/debounce";
 
 export default function ClientSelect() {
   const [searchTerm, setSearchTerm] = useState("");
-  console.log(searchTerm);
+  console.log("searchTerm", searchTerm);
   //query to get suggestions
   const { data, loading } = useQuery(GET_CLIENTS);
-
+  console.log("data: ", data);
   const setSearchTermDebounced = debounce(setSearchTerm, 500);
 
   return (
--- frontend/react-graphql-apollo/src/pages/recibos/venta/grapql/query.js
+++ frontend/react-graphql-apollo/src/pages/recibos/venta/grapql/query.js
@@ -1,10 +1,12 @@
 import { gql } from "@apollo/client";
 
 export const GET_CLIENTS = gql`
-  query clientes {
-    id
-    nombre
-    telefono
-    dirrecion
+  query getClientes {
+    clientes {
+      id
+      nombre
+      telefono
+      dirrecion
+    }
   }
 `;
