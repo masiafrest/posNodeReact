689033b49e87c66532e358072c570ae9c86cd1d6
masiafrest <masiafrest@gmail.com>
2021-07-18 12:08:00 -0500

    add notistack to item added

--- frontend/react-graphql-apollo/src/pages/item/components/AddBtn.jsx
+++ frontend/react-graphql-apollo/src/pages/item/components/AddBtn.jsx
@@ -2,16 +2,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { pushLinea } from "../../../redux/features/reciboSlice";
 import { IconButton } from "@material-ui/core";
 import { AddShoppingCart as AddShoppingCartIcon } from "@material-ui/icons/";
+import { useSnackbar } from "notistack";
 
 export default function AddBtn({ item, reciboTipo }) {
   const dispatch = useDispatch();
+  const lineas = useSelector((state) => state.recibo.venta.lineas);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   return (
     <IconButton
       onClick={() => {
         const newLinea = { ...item };
-        newLinea.tipo = reciboTipo;
-        newLinea.qty = 1;
-        dispatch(pushLinea(newLinea));
+        const hasId = lineas.some((linea) => linea.id === newLinea.id);
+        if (!hasId) {
+          newLinea.tipo = reciboTipo;
+          newLinea.qty = 1;
+          dispatch(pushLinea(newLinea));
+          enqueueSnackbar("item agregado", {
+            variant: "success",
+          });
+        } else {
+          enqueueSnackbar("item ya esta agregado", {
+            variant: "warning",
+          });
+        }
       }}
     >
       <AddShoppingCartIcon />
