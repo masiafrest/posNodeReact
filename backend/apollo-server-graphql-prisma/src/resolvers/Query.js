a0708e2b02ae6c1aca66dcdc4420b4db27a45fe8
masiafrest <masiafrest@gmail.com>
2021-08-02 19:05:50 -0500

    add funtion to authenticated

--- backend/apollo-server-graphql-prisma/src/resolvers/Query.js
+++ backend/apollo-server-graphql-prisma/src/resolvers/Query.js
@@ -4,6 +4,13 @@ const { clientes } = require("./controllers/cliente");
 const { items, item } = require("./controllers/item");
 const { ventas } = require("./controllers/recibos/venta");
 
+const authenticated = (next) => (root, args, context, info) => {
+  if (!context.currentUser) {
+    throw new Error(`Unauthenticated!`);
+  }
+
+  return next(root, args, context, info);
+};
 module.exports = {
   items,
   item,
