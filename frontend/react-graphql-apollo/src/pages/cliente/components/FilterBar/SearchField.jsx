5237875835b13f97093fe9e7d832b89651acca70
pcLinux <masiafrest@gmail.com>
2021-08-15 17:36:26 -0500

    fix bug

--- frontend/react-graphql-apollo/src/pages/cliente/components/FilterBar/SearchField.jsx
+++ frontend/react-graphql-apollo/src/pages/cliente/components/FilterBar/SearchField.jsx
@@ -64,7 +64,7 @@ export default function SearchField({
             selected: false,
           },
         });
-        enqueueSnackbar(`cliente ${v.nombre} borrado`, {
+        enqueueSnackbar(`cliente borrado`, {
           variant: "warning",
         });
       }
