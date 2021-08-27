c332c8eb72ebf9c6a2a919ae8b6c804367ca0826
masiafrest <masiafrest@gmail.com>
2021-08-26 19:11:13 -0500

    fix close snacbar behaviour

--- frontend/react-graphql-apollo/src/components/CloseSnackBar.jsx
+++ frontend/react-graphql-apollo/src/components/CloseSnackBar.jsx
@@ -1,15 +1,12 @@
-import { IconButton } from '@material-ui/core'
-import CancelIcon from '@material-ui/icons/CancelPresentation';
-import { useSnackbar } from 'notistack'
+import { IconButton } from "@material-ui/core";
+import CancelIcon from "@material-ui/icons/CancelPresentation";
+import { useSnackbar } from "notistack";
 
 export default function CloseSnackBar({ key }) {
-    const { closeSnackbar } = useSnackbar()
-    return (
-        <IconButton
-            onClick={() => closeSnackbar(key)}
-        >
-            <CancelIcon />
-        </IconButton>
-
-    )
+  const { closeSnackbar } = useSnackbar();
+  return (
+    <IconButton onClick={() => closeSnackbar(key)}>
+      <CancelIcon />
+    </IconButton>
+  );
 }
