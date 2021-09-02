6865ac1f1772781e66f506c2bb0588364da0d4c2
masiafrest <masiafrest@gmail.com>
2021-09-02 18:05:51 -0500

    add darkmode

--- frontend/react-graphql-apollo/src/App.js
+++ frontend/react-graphql-apollo/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import CloseSnackBar from "./components/CloseSnackBar";
 
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 //redux
 import store, { persistor } from "./redux/store";
 import { Provider } from "react-redux";
@@ -24,33 +28,50 @@ import { checkToken } from "./utils";
 checkToken(store);
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: "dark",
+        },
+      }),
+    [prefersDarkMode]
+  );
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <SnackbarProvider
-          maxSnack={2}
-          action={(key) => {
-            return <CloseSnackBar key={key} />;
-          }}
-        >
-          <Router>
-            <NavBar />
-            <Switch>
-              <Container>
-                <AuthRoute exact path="/" component={Item} />
-                <Route path="/login" component={Login} />
-                <AuthRoute path="/item" component={Item} />
-                <AuthRoute path="/item/new/:page" component={Item} />
-                <AuthRoute path="/venta" component={Venta} />
-                <AuthRoute path="/devolucion" component={Devolucion} />
-                <AuthRoute path="/cliente" component={Cliente} />
-                <AuthRoute path="/cliente/new/:page" component={Cliente} />
-                <AuthRoute path="/categoria" component={Categoria} />
-                <AuthRoute path="/categoria/new/:page" component={Categoria} />
-              </Container>
-            </Switch>
-          </Router>
-        </SnackbarProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <SnackbarProvider
+            maxSnack={2}
+            action={(key) => {
+              return <CloseSnackBar key={key} />;
+            }}
+          >
+            <Router>
+              <NavBar />
+              <Switch>
+                <Container>
+                  <AuthRoute exact path="/" component={Item} />
+                  <Route path="/login" component={Login} />
+                  <AuthRoute path="/item" component={Item} />
+                  <AuthRoute path="/item/new/:page" component={Item} />
+                  <AuthRoute path="/venta" component={Venta} />
+                  <AuthRoute path="/devolucion" component={Devolucion} />
+                  <AuthRoute path="/cliente" component={Cliente} />
+                  <AuthRoute path="/cliente/new/:page" component={Cliente} />
+                  <AuthRoute path="/categoria" component={Categoria} />
+                  <AuthRoute
+                    path="/categoria/new/:page"
+                    component={Categoria}
+                  />
+                </Container>
+              </Switch>
+            </Router>
+          </SnackbarProvider>
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   );
