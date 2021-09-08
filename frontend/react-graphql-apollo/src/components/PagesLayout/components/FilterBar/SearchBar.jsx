f69b43f30b6c6a831aac5fa7dc67b4e41c177850
masiafrest <masiafrest@gmail.com>
2021-09-08 18:30:01 -0500

    change tsx to jsx

--- frontend/react-graphql-apollo/src/components/PagesLayout/components/FilterBar/SearchBar.jsx
+++ frontend/react-graphql-apollo/src/components/PagesLayout/components/FilterBar/SearchBar.jsx
@@ -1,42 +1,41 @@
-
 import { useState, useContext } from "react";
-import { FilterBarState } from '../../../PagesLayout'
+import { FilterBarState } from "../..";
 
 import { TextField, InputAdornment } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 export default function SearchItem() {
-	const { filterState, pageState } = useContext(FilterBarState);
-	const setPage = pageState[1];
-	const [filter, setFilter] = filterState;
-	const [term, setTerm] = useState(filter);
+  const { filterState, pageState } = useContext(FilterBarState);
+  const setPage = pageState[1];
+  const [filter, setFilter] = filterState;
+  const [term, setTerm] = useState(filter);
 
-	const handleChange = (event) => {
-		setTerm(event.target.value.toUpperCase());
-	};
+  const handleChange = (event) => {
+    setTerm(event.target.value.toUpperCase());
+  };
 
-	return (
-		<form
-			onSubmit={(e) => {
-				e.preventDefault();
-				console.log("clieck term:", term);
-				setFilter(term);
-				setPage(1);
-				setTerm("");
-			}}
-		>
-			<TextField
-				type="search"
-				value={term}
-				onChange={handleChange}
-				InputProps={{
-					startAdornment: (
-						<InputAdornment position="start">
-							<SearchIcon />
-						</InputAdornment>
-					),
-				}}
-			/>
-		</form>
-	);
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log("clieck term:", term);
+        setFilter(term);
+        setPage(1);
+        setTerm("");
+      }}
+    >
+      <TextField
+        type="search"
+        value={term}
+        onChange={handleChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
+  );
 }
