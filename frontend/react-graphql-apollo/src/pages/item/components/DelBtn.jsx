a4bd3da2c3ea1f66ac5df25a6296b09f794dbcfd
masiafrest <masiafrest@gmail.com>
2022-01-17 10:45:16 -0500

    -madd useToggle hooks, modal para avisar que vas a borrar

--- frontend/react-graphql-apollo/src/pages/item/components/DelBtn.jsx
+++ frontend/react-graphql-apollo/src/pages/item/components/DelBtn.jsx
@@ -1,12 +1,24 @@
 import { useMutation } from "@apollo/client";
 import { DEL_ITEM } from "../graphql/mutation";
-import { IconButton } from "@material-ui/core";
+
+import useToggle from "../../../hooks/useToggle";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  DialogTitle,
+  Button,
+  IconButton,
+} from "@material-ui/core";
 
 import DeleteIcon from "@material-ui/icons/Delete";
 import { useSnackbar } from "notistack";
 
 export default function DelBtn({ id, paths }) {
   const { enqueueSnackbar } = useSnackbar();
+  const [isOpen, toggleIsOpen] = useToggle(false);
 
   const [delItem, { loading, error }] = useMutation(DEL_ITEM, {
     update(cache, { data: { delItem } }) {
@@ -22,6 +34,7 @@ export default function DelBtn({ id, paths }) {
     },
     onCompleted(data) {
       const { descripcion } = data.delItem;
+      toggleIsOpen();
       enqueueSnackbar(`item ${descripcion} eliminado`, {
         variant: "success",
       });
@@ -34,11 +47,34 @@ export default function DelBtn({ id, paths }) {
   });
 
   return (
-    <IconButton
-      disabled={loading}
-      onClick={() => delItem({ variables: { id: id * 1, paths } })}
-    >
-      <DeleteIcon />
-    </IconButton>
+    <>
+      <IconButton
+        disabled={loading}
+        // onClick={() => delItem({ variables: { id: id * 1, paths } })}
+        onClick={toggleIsOpen}
+      >
+        <DeleteIcon />
+      </IconButton>
+      <Dialog open={isOpen} close={toggleIsOpen}>
+        <DialogTitle>Estas seguro que vas a borrarlo?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            esta accion no se puede revertir o deshacer, estas seguro que desea
+            borrar?.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              delItem({ variables: { id: id * 1, paths } });
+              // toggleIsOpen();
+            }}
+          >
+            Borrar
+          </Button>
+          <Button onClick={toggleIsOpen}>Cancelar</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
